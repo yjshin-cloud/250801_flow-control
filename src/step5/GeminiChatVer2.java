a331e9c829f5e3a297e1e40e8d785c8ad64b99a2
yjshin-cloud <yjshin.cloud@gmail.com>
2025-08-01 17:08:07 +0900

    흐름제어 + AI!

--- src/step5/GeminiChatVer2.java
+++ src/step5/GeminiChatVer2.java
@@ -56,7 +56,7 @@ public class GeminiChatVer2 {
             // 이전사항? 어디에다가 저장할까요?
 
             // 여기서 요청을 보낼 예정.
-            String prompt = "%d글자 길이 이하의 스무고개용 띄어쓰기 없는 한글 단어 1개를 과정 없이 결과만 출력해줘.".formatted(rd.nextInt(3, 6)); // 3~5글자 단어
+            String prompt = "한글 기준 %d글자 길이의 스무고개용 띄어쓰기 없는 한글 단어 1개를 과정 없이, 마크다운 같은 꾸미는 옵션 없이, 오로지 단어 결과만 출력해줘.".formatted(rd.nextInt(3, 6)); // 3~5글자 단어
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(GEMINI_URL))
                     .headers("Content-Type", "application/json",
@@ -83,6 +83,52 @@ public class GeminiChatVer2 {
             // String twenty = "고양이";
 
             System.out.println("스무고개 답은 %s입니다.".formatted(aiResult));
+
+            String[] aiQuestionArr = new String[20];
+            for (int i = 0; i < 20; i++) {
+                System.out.print((i+1) + "번째 질문을 해주세요! : ");
+                String userQuestion = sc.nextLine();
+                String prompt2 = "";
+                if (i == 0) {
+                    prompt2 = "[%s] 는 정답인 [%s] 에 대한 질문이야. 예/아니오라고 먼저 대답한 뒤에 부가적인 설명을 해줘. 절대로 정답인 [%s]를 언급하지 마. 과정 없이 결과만 출력해줘.".formatted(userQuestion, aiResult, aiResult);
+                } else {
+                    String prevQuestion = "";
+                    for (int j = 0; j < i; j++) { // 이전 질문 목록을 묶고.
+                        prevQuestion += aiQuestionArr[j];
+                        prevQuestion += ", ";
+                    }
+                    System.out.println("이전 질문 : " + prevQuestion);
+                    prompt2 = "[%s] 는 정답인 [%s] 에 대한 질문이야. 예/아니오라고 먼저 대답한 뒤에 부가적인 설명을 해줘. 이전 답변인 [%s]와 답변이 중복된다면 비슷한 질문을 하고 있다고 언급해줘. 절대로 정답인 [%s]를 언급하지 마. 과정 없이 결과만 출력해줘.".formatted(userQuestion, aiResult, prevQuestion, aiResult);
+                }
+                // 3~5글자 단어
+                HttpRequest request2 = HttpRequest.newBuilder()
+                        .uri(URI.create(GEMINI_URL))
+                        .headers("Content-Type", "application/json",
+                                "X-goog-api-key", GEMINI_API_KEY)
+                        .POST(HttpRequest.BodyPublishers.ofString(
+                                bodyTemplate.formatted(prompt2)
+                        ))
+                        .build();
+                String aiResult2 = "";
+                try {
+                    HttpResponse<String> response = client.send(request2, HttpResponse.BodyHandlers.ofString());
+//                System.out.println(response.body());
+                    aiResult2 = response.body()
+                            .split("\"text\": \"")[1] // 0, 1, 2....
+                            .split("}")[0]
+                            .replace("\\n", "")
+                            .replace("\"", "")
+                            .trim();
+                    System.out.println(aiResult2);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage()); // 에러가 날 경우 해당 메시지를 확인 (429? 403?)
+                    // 403 : 키를 잘 못넣은거고
+                    // 429 : 키를 너무 많이 쓴 것
+                }
+                System.out.println("AI의 대답 : " + aiResult2);
+                // 이게 빠짐;;;;
+                aiQuestionArr[i] = userQuestion;
+            }
 
 //            System.out.print("원하시는 명령을 입력해주세요. (시작, 종료) : ");
 //            String cmd = sc.nextLine();
